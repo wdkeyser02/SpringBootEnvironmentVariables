a86afbdfb3dfdfbe230165d020090f561eafc3c6
willydekeyser <ma074172@skynet.be>
2023-09-17 18:47:59 +0200

    Update application.properties & HomeController

---
+++ SpringBootEnvironmentVariables/src/main/java/willydekeyser/controller/HomeController.java
@@ -0,0 +1,20 @@
+package willydekeyser.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@Value("${spring.boot.tutorial.variable01}")
+	String variable01;
+	
+	@Value("${spring.boot.tutorial.variable02}")
+	String variable02;
+	
+	@GetMapping("/")
+	public String home() {
+		return "Spring Boot Tutorial Variables: " + variable01 + " - " + variable02;
+	}
+}
